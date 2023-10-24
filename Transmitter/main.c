1d31bbc8d8ba43de2c16c77bdbe41a2c95dc4908
siphenkosi10 <86193819+siphenkosi10@users.noreply.github.com>
2023-10-24 23:35:18 +0200

    Create main.c

---
+++ Transmitter/main.c
@@ -0,0 +1,414 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+#include <stdio.h>
+#include "stm32f0xx.h"
+#include <lcd_stm32f0.c>
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc;
+TIM_HandleTypeDef htim3;
+
+uint32_t delay_t = 500;
+uint32_t adc_val;
+uint8_t flag = 0;
+uint32_t prev_press_time = 0;
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC_Init(void);
+static void MX_TIM3_Init(void);
+
+void EXTI0_1_IRQHandler(void);
+void writeLCD(char *char_in);
+uint32_t pollADC(void);
+uint32_t ADCtoCCR(uint32_t adc_val);
+void sampleADCValue(void);
+void transmit(uint32_t data);
+
+int main(void)
+{
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC_Init();
+  MX_TIM3_Init();
+
+  init_LCD();
+
+  writeLCD("TRANSMITTER");
+
+  while (1)
+  {
+	  if (flag == 1){
+		  transmit(adc_val);
+	  }
+  }
+}
+
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  {
+  }
+  LL_RCC_HSI_Enable();
+
+   /* Wait till HSI is ready */
+  while(LL_RCC_HSI_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+  LL_RCC_HSI14_Enable();
+
+   /* Wait till HSI14 is ready */
+  while(LL_RCC_HSI14_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_HSI14_SetCalibTrimming(16);
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
+  {
+
+  }
+  LL_SetSystemCoreClock(8000000);
+
+   /* Update the time base */
+  if (HAL_InitTick (TICK_INT_PRIORITY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  LL_RCC_HSI14_EnableADCControl();
+}
+
+/**
+  * @brief ADC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC_Init(void)
+{
+
+  /* USER CODE BEGIN ADC_Init 0 */
+  /* USER CODE END ADC_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC_Init 1 */
+
+  /* USER CODE END ADC_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc.Instance = ADC1;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc.Init.LowPowerAutoWait = DISABLE;
+  hadc.Init.LowPowerAutoPowerOff = DISABLE;
+  hadc.Init.ContinuousConvMode = DISABLE;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.DMAContinuousRequests = DISABLE;
+  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_6;
+  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC_Init 2 */
+  ADC1->CR |= ADC_CR_ADCAL;
+  while(ADC1->CR & ADC_CR_ADCAL);			// Calibrate the ADC
+  ADC1->CR |= (1 << 0);						// Enable ADC
+  while((ADC1->ISR & (1 << 0)) == 0);		// Wait for ADC ready
+  /* USER CODE END ADC_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 47999;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOF);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
+
+  /**/
+  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTA, LL_SYSCFG_EXTI_LINE0);
+
+  /**/
+  LL_GPIO_SetPinPull(Button0_GPIO_Port, Button0_Pin, LL_GPIO_PULL_UP);
+
+  /**/
+  LL_GPIO_SetPinMode(Button0_GPIO_Port, Button0_Pin, LL_GPIO_MODE_INPUT);
+
+  /**/
+  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
+  EXTI_InitStruct.LineCommand = ENABLE;
+  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
+  LL_EXTI_Init(&EXTI_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED7_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED7_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+  HAL_NVIC_SetPriority(EXTI0_1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+void EXTI0_1_IRQHandler(void)
+{
+	//debouncing...
+	if ((prev_press_time == 0)||((HAL_GetTick() - prev_press_time) > 1000)){
+		sampleADCValue();
+		flag = 1;
+		prev_press_time = HAL_GetTick();
+	}
+
+	HAL_GPIO_EXTI_IRQHandler(Button0_Pin); // Clear interrupt flags
+}
+
+void writeLCD(char *char_in){
+	lcd_command(CLEAR);
+	lcd_putstring(char_in);
+}
+
+void sampleADCValue(void){
+	// Read POT1 Value
+	adc_val = pollADC();
+
+	// Write POT1 value to LCD
+	char adc_str[16]; // string array buffer
+	/* snprrintf(char *str, size_t size, const char *format, value)
+	 * Formats and stores a series of characters and values in the
+	 * array buffer
+	 * Source: https://www.geeksforgeeks.org/snprintf-c-library/
+	*/
+
+	snprintf(adc_str, sizeof(adc_str), "%lu", adc_val);
+	writeLCD(adc_str);
+
+}
+
+void transmit(uint32_t data){
+	//writeLCD("Waiting...");
+	while(HAL_GPIO_ReadPin(GPIOB, LED7_Pin) == GPIO_PIN_RESET){
+
+	}
+	writeLCD("Transmitting...");
+
+	HAL_Delay(delay_t/100);
+	//convert sampled value to binary sequence and transmit
+
+	for (int i=0; i < 12; i++){
+		uint32_t bit = (data & 1);
+		if (bit == 1){
+			HAL_GPIO_WritePin(GPIOB, LED7_Pin, GPIO_PIN_SET);
+		}else{
+			HAL_GPIO_WritePin(GPIOB, LED7_Pin, GPIO_PIN_RESET);
+		}
+		data >>=1;
+		HAL_Delay(delay_t/5);
+	}
+
+	for (int i=0; i<10; i++){
+		HAL_GPIO_TogglePin(GPIOB, LED7_Pin);
+		HAL_Delay(delay_t/50);
+	}
+	writeLCD("Done");
+
+	char* packets = "1100"; //12 bit were transmitted
+
+	for (int i=0; i<4; i++){
+		uint8_t bit = packets[i] - '0';
+		if (bit == 1){
+			HAL_GPIO_WritePin(GPIOB, LED7_Pin, GPIO_PIN_SET);
+		}else{
+			HAL_GPIO_WritePin(GPIOB, LED7_Pin, GPIO_PIN_RESET);
+		}
+		HAL_Delay(delay_t);
+	}
+
+	HAL_GPIO_WritePin(GPIOB, LED7_Pin, GPIO_PIN_RESET);
+
+	flag = 0;
+}
+// Get ADC value
+uint32_t pollADC(void){
+	HAL_ADC_Start(&hadc); // Configure and Start ADC Conversion
+	if (HAL_ADC_PollForConversion(&hadc, HAL_MAX_DELAY) == 0)
+	{
+		adc_val = HAL_ADC_GetValue(&hadc);
+	}
+	return adc_val;
+}
+
+// Calculate PWM CCR value
+uint32_t ADCtoCCR(uint32_t adc_val){
+	/*	Ranges:
+	 *  ADC Values: 0 to 4095(adc_max_val)
+	 *  PWM: 0 to 47999(pwm_max_val)
+	 *
+	 *  CCR = (adc_val * pwm_max_val)/adc_max_val
+	 */
+	return (adc_val * 47999)/4095;
+}
+
+void ADC1_COMP_IRQHandler(void)
+{
+	adc_val = HAL_ADC_GetValue(&hadc); // read adc value
+	HAL_ADC_IRQHandler(&hadc); //Clear flags
+}
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
